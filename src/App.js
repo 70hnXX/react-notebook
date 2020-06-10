fd0e0052dfacd57f2b67040dd26e59e948727bae
70hnXX <iloveguoguo92>
2020-06-11 07:41:41 +0800

    添加数据双向绑定的demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import CreateElementDemo from './demos/CreateElementDemo'
+import DataBind from './demos/DataBind'
 
 class App extends Component {
     render() {
         return ( 
           <div >
-            Hello World 
+            <DataBind/>
           </div>
         )
     }
