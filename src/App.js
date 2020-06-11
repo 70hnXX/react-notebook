432806e40b941ac773f3dc7da5facc70c4432be5
wuyangjiacuo <wuyangjiacuo@gmail.co>
2020-06-11 09:38:36 +0800

    修改文件依赖

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
-import CreateElementDemo from './demos/CreateElementDemo'
-import DataBind from './demos/DataBind'
 
 class App extends Component {
     render() {
-        return ( <
-            div >
-            hello react <
-            /div>
+        return ( 
+          <div >
+            hello react 
+          </div>
         )
     }
 }
